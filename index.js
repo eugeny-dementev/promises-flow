34c222f5fc553301f1d1d22354809c75087721d2
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-05-22 17:50:26 +0800

    first function version

---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * Revieve the map object with promises or flow structures objects
+ * @example promisesFlow
+ *   .run({
+ *     elems: Promise.resolve([1, 2, 3]),
+ *     incremented: {
+ *       deps: ['elems'],
+ *       cb: function elemsHandler ({ elems }) {
+ *         return elems.map(elem => elem + 1);
+ *       },
+ *     },
+ *     decremented: {
+ *       deps: ['incremented'],
+ *       cb: function incrementedElemsHandler ({ incremented }) {
+ *         return elems.map(elem => elem - 2);
+ *       },
+ *     }
+ *   })
+ *   .then(({ elems, incremented, decremented }) => { })
+ *   .catch((error) => `handle error from any of promise`);
+ */
+exports.run = function (mapObject) {
+  const keys = Object.keys(mapObject);
+  const results = keys
+    .reduce((result, key) => Object
+      .assign(result, {
+        [key]: prepareOpenAPIPromise(),
+      }), {});
+
+  Object
+    .keys(mapObject)
+    .forEach((name) => {
+      const target = mapObject[name];
+
+      if (target instanceof Promise) {
+        results[name]
+          .resolve(target);
+
+        return;
+      } else if (target.deps.length) {
+        const deps = target.deps
+          .filter((dep) => dep != name);
+
+        const depsPromises = deps
+          .map((depName) => Promise
+            .resolve()
+            .then(() => results[depName]
+              .then((result) => ({
+                [depName]: result,
+              }))));
+
+        const promise = Promise
+          .all(depsPromises)
+          .then((depsResults) => Object
+            .assign(...depsResults))
+          .then(target.cb);
+
+        results[name]
+          .resolve(promise);
+      }
+    });
+
+  return Promise
+    .all(Object
+      .keys(results)
+      .map((key) => results[key]
+        .then((result) => ({
+          [key]: result,
+        }))))
+    .then((results) => Object
+      .assign(...results));
+}
+
+function prepareOpenAPIPromise () {
+  let resolve;
+  let reject;
+
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return {
+    resolve,
+    reject,
+    then: promise.then.bind(promise),
+  };
+}
---
+++ test/index.js
@@ -0,0 +1,162 @@
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const assert = require('assert');
+
+const {
+  test,
+} = lab;
+
+const promisesFlow = require('../index');
+
+test('should return object with same properties as recieved', (done) => {
+  const mapObject = {
+    elems: Promise.resolve([1, 2, 3]),
+    delayed: delay(100),
+    depended: {
+      deps: ['elems', 'delayed'],
+      cb ({ elems, delayed }) {
+        return null;
+      },
+    }
+  };
+
+  const taskKeys = Object
+    .keys(mapObject)
+    .sort();
+
+  promisesFlow
+    .run(mapObject)
+    .then((result) => {
+      const resultKeys = Object
+        .keys(result)
+        .sort();
+
+      assert.equal(resultKeys.length, taskKeys.length);
+
+      resultKeys
+        .forEach((key, index) => assert.equal(key, taskKeys[index]));
+
+      done();
+    })
+    .catch(done);
+});
+
+test('should allow deps to other promises elements', (done) => {
+  const mapObject = {
+    one: Promise.resolve(1),
+    two: delay(100, 2),
+    three: {
+      deps: ['one', 'two'],
+      cb ({ one, two }) {
+        return one + two;
+      },
+    }
+  };
+
+  promisesFlow
+    .run(mapObject)
+    .then((results) => {
+      assert.deepEqual(results, {
+        one: 1,
+        two: 2,
+        three: 3,
+      });
+
+      done();
+    })
+    .catch(done);
+});
+
+test('should allow multichained deps for promises elements', (done) => {
+  const mapObject = {
+    one: Promise.resolve(1),
+    two: {
+      deps: ['one'],
+      cb ({ one }) {
+        return one + 1;
+      }
+    },
+    three: {
+      deps: ['one', 'two'],
+      cb ({ one, two }) {
+        return one + two;
+      }
+    },
+  };
+
+  promisesFlow
+    .run(mapObject)
+    .then((results) => {
+      assert.deepEqual(results, {
+        one: 1,
+        two: 2,
+        three: 3,
+      });
+
+      done();
+    })
+    .catch(done);
+});
+
+test('should allow return promise instance in cb function', (done) => {
+  const expected = {
+    one: 1,
+    two: 2,
+  };
+  const mapObject = {
+    one: Promise.resolve(1),
+    two: {
+      deps: ['one'],
+      cb ({ one }) {
+        return delay(100, one + one);
+      },
+    },
+  };
+
+  promisesFlow
+    .run(mapObject)
+    .then((result) => {
+      assert.deepEqual(result, expected);
+
+      done();
+    })
+    .catch(done);
+});
+
+test('should run all promises as soon as possible', (done) => {
+  const runOrder = [];
+  const expectedOrder = [2, 3, 1];
+
+  function pushOrderValue (value) {
+    runOrder
+      .push(value);
+
+    return value;
+  }
+
+  const mapObject = {
+    one: delay(200, 1, pushOrderValue),
+    two: delay(100, 2, pushOrderValue),
+    three: {
+      deps: ['two'],
+      cb () {
+        return delay(50, 3, pushOrderValue);
+      }
+    },
+  };
+
+  promisesFlow
+    .run(mapObject)
+    .then(() => {
+      assert.deepEqual(runOrder, expectedOrder);
+
+      done();
+    })
+    .catch(done);
+});
+
+function delay (timeout, value, cb = (value) => value) {
+  return new Promise((resolve) => setTimeout(resolve, timeout, value))
+    .then(cb);
+}
